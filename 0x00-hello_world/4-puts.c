ba7ac83a3bcbf2982697fe0ab7b837787b44162d
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-05 20:36:06 +0000

    corretion of commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-*main:function that have out a integer.
-*puts:function that show a string of text in screen. 
-*
-*/
+ *main - function that have out a integer
+ *Return: 0
+ */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
