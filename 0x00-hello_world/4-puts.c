56ad783bc09e4b646412354499bc9dc617787910
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-05 20:19:20 +0000

    adding file task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main:function that have out a integer.
+*puts:function that show a string of text in screen. 
+*
+*/
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
