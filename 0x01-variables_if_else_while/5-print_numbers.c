4e4ed6cc304268af96fa6f0075bd5de71a237e57
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-06 20:52:18 +0000

    adding task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb < 10; numb++)
+	{
+		printf("%d", numb);
+	}
+	putchar('\n');
+
+	return (0);
+}
