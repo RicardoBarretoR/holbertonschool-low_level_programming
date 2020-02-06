7f3f1ab7202976ea5916a18c7f6c37b00095b702
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-06 22:54:17 +0000

    add task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0
+ */
+int main(void)
+{
+	int tebahpla;
+
+	for (tebahpla = 'z'; tebahpla >= 'a'; tebahpla--)
+	{
+		putchar(tebahpla);
+	}
+	putchar('\n');
+
+	return (0);
+}
