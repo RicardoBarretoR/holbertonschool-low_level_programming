210cde3332ca8246516ba11d91192a58bba3242b
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-11 20:12:43 +0000

    adding task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *jack_bauer - function that prints every minute of the day
+ *Return:
+ */
+void jack_bauer(void)
+{
+	int m1;
+	int m2;
+	int h1;
+	int h2;
+
+	for (h2 = '0'; h2 <= '2'; h2++)
+	{
+		for (h1 = '0'; h1 <= '9'; h1++)
+		{
+			for (m2 = '0'; m2 <= '5'; m2++)
+			{
+				for (m1 = '0'; m1 <= '9'; m1++)
+				{
+					if (h2 <= '2' && h1 <= '3' && m2 <= '5')
+					{
+						_putchar(h2);
+						_putchar(h1);
+						_putchar(':');
+						_putchar(m2);
+						_putchar(m1);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
