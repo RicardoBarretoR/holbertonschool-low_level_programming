708f817569ee1e01e34cfd4dbdd28659645f933a
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-11 23:04:43 +0000

    adding task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,19 +14,21 @@ void jack_bauer(void)
 	{
 		for (h1 = '0'; h1 <= '9'; h1++)
 		{
+			if (h2 == '2' && h1 == '4')
+			{
+				break;
+			}
+
 			for (m2 = '0'; m2 <= '5'; m2++)
 			{
 				for (m1 = '0'; m1 <= '9'; m1++)
 				{
-					if (h2 <= '2' && h1 <= '3' && m2 <= '5')
-					{
-						_putchar(h2);
-						_putchar(h1);
-						_putchar(':');
-						_putchar(m2);
-						_putchar(m1);
-						_putchar('\n');
-					}
+					_putchar(h2);
+					_putchar(h1);
+					_putchar(':');
+					_putchar(m2);
+					_putchar(m1);
+					_putchar('\n');
 				}
 			}
 		}
