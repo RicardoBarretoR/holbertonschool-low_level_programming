6cc4d93e72ffb548bc1665f0f91cd9fe7be319d0
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-12 00:27:30 +0000

    adding task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *add - function that adds two integers and returns the result
+ *@n1: integer
+ *@n2: integer
+ *Return: 0
+ */
+int add(int n1, int n2)
+{
+	int sum = 0;
+
+	sum = n1 + n2;
+	return (sum);
+}
