217a34da731c5f90231cb794f8cf8cfda7bdb3e2
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-13 15:20:53 +0000

    adding directory and file task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isupper - function that checks for uppercase character
+ *@c: variable of a character
+ *Return: 1 - 0
+ */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
