2486f68352b09411a51e71c71fcd49832c86d5a9
RicardoBarretoR <ricardobarrodriguez@hotmail.com>
2020-02-13 23:33:37 -0500

    adding task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - function that prints the numbers from 1 to 100
+ *
+ */
+ int main(void)
+{
+	int i;
+	int div1 = 0;
+	int div2 = 0;
+		
+	for (i = 1; i <= 100; i++)
+	{
+		div1 = i % 3;
+		div2 = i % 5;
+
+		if (div1 == 0 && div2 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if(div1 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (div2 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+}
