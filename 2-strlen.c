b1a461fea0e6d2c9db70713f54054d4c17792ae6
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-21 00:21:23 +0000

    adding task 2

---
+++ 2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_strlen - function that returns the length of a string
+ *@s: pointer integer
+ *Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
