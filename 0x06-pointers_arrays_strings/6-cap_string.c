d0264a2783dd0a47837a7c0687c69e0554b9d252
RicardoBarretoR <ricardobarrodriguez@hotmail.com>
2020-02-23 15:24:29 -0500

    adding task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,10 +10,6 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == '\t')
-		{
-			s[i] = 32;
-		}
 		if (s[i] == '.' && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 		{
 			s[i + 1] -= 32;
