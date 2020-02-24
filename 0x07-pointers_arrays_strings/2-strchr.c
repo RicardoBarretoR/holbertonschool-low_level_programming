924503ac7db9c43be7e82eafcb044e2c279669e2
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-24 22:12:34 +0000

    fixing task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
+		if (!*s++)
+			return (0);
 	}
-	return (0);
+	return (s);
 }
