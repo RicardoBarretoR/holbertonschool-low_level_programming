5e0c75ffdb14c37d4b4c0cc6c383bebd94597080
RicardoBarretoR <ricardobarrodriguez@hotmail.com>
2020-02-24 23:22:14 -0500

    fixing task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strpbrk - function that searches a string for any of a set bytes
+ * @s: address of memory
+ * @accept: string
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j])
+			return (s + i);
+	}
+	return (0);
+}
