2c06f9067860498b706284282996533e6e78eeb5
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-02-29 17:46:53 +0000

    adding task 3 and update file .h

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - function that multiplies two numbers
+ *@argc: Recount of the arguments provided
+ *@argv: Matrix of pointers to the chains that are arguments
+ *Return: resul
+ */
+int main(int argc, char *argv[])
+{
+	int resul = 0;
+	int num1 = 0;
+	int num2 = 0;
+	int i;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		num1 = _atoi(argv[1]);
+		num2 = _atoi(argv[2]);
+	}
+
+	resul = num1 * num2;
+	printf("%d\n", resul);
+
+	return (0);
+}
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,7 +1,9 @@
-#ifndef
-#define
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
+#include <math.h>
 int _putchar(char c);
+int _atoi(char *s);
 
 #endif
