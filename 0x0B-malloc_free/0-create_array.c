b3b253f033d706abc553fe45b2310a7b438ecff3
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-03 20:40:00 +0000

    fixing task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,17 +17,15 @@ char *create_array(unsigned int size, char c)
 	{
 		return ('\0');
 	}
-
-	ch = malloc(size * sizeof(char));
-
+	ch = malloc(size);
+	if (ch == NULL)
+	{
+		return ('\0');
+	}
 	for (i = 0; i < size; i++)
 	{
 		ch[i] = c;
 	}
-	if (!*ch)
-	{
-		return ('\0');
-	}
-
+	
 	return (ch);
 }
