7acc8d41d7788779f6b4196b3df098aad9bc5e04
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-05 21:59:30 +0000

    fixing task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int l2;
 
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+	{
+		return ("");
+	}
+	if (s2 == NULL)
 	{
 		return ("");
 	}
