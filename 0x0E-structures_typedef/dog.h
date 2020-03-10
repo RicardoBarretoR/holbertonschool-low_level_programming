0c9f053196e0f3d87f0be860575129d14fd70df9
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-10 00:54:28 +0000

    fixing task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,33 +1,23 @@
-#ifndef DOG_T
-#define DOG_T
+#ifndef DOG_H
+#define DOG_H
 /**
- *struct dog_t - define a new type
+ *struct dog - define a new type
  *@name: name of dog
  *@age: old dog
  *@owner: owner of dog
+ *typedef struct dog_t - define a new data type
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
-
-typedef struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-}dog;
-
+} dog_t;
 #include <stdio.h>
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*
-*dog_t *new_dog(char *name, float age, char *owner);
-*void free_dog(dog_t *d);
-*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
