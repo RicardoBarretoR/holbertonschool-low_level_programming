cc36d1a0765867dbe28f17c2b817b49b4c337806
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-10 00:33:44 +0000

    adding task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG_T
+#define DOG_T
 /**
- *struct dog - define a new type
+ *struct dog_t - define a new type
  *@name: name of dog
  *@age: old dog
  *@owner: owner of dog
@@ -12,6 +12,17 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
+
+#include <stdio.h>
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
