7c7162cb921e9b6208ab9222dd44846fcb3bbf04
RicardoBarretoR <ricardobarrodriguez@hotmail.com>
2020-03-16 00:18:23 -0500

    fixing file .h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
+typedef struct {
+	char *fto;
+	void (*p)(va_list);
+} specif_fto;
 
 #endif
