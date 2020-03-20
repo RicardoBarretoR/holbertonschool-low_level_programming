a07a70a581dd53eac17cf892933e5bf4d3b299f1
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-20 01:11:04 +0000

    adding task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *size_t print_list - prints all the elements of a list_t list
+ *@h: pointer that point at address of memory date
+ *Return: number of node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("%d %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+	
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+ Foundations - Low-level programming & Algorithm  Data structures and Algorithms
+
+ 0x12. C - Singly linked lists
+
+
+
