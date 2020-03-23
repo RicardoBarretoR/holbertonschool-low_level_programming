fcf1d93bdbcbee107ab62180f4f726de766506a3
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-23 23:14:21 +0000

    adding files and task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *print_listint - prints all elements of a listint_t list
+ *@h: pointer that point address of memory of head
+ *Return: the number of node
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num_n = 0;
+
+	while (h != '\0')
+	{
+		if ((*h).n != '\0')
+			printf("%d\n", h->n);
+		h = (*h).next;
+		num_n++;
+	}
+	return (num_n);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+ Foundations - Low-level programming & Algorithm  Data structures and Algorithms
+
+ 0x13. C - More singly linked lists
+
