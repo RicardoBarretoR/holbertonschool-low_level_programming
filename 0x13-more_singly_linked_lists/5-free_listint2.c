298e62348100eb8980befd803cbbfec84bc4ac9e
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-25 03:44:04 +0000

    fixing task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
 
 	while (*head != NULL)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
 	}
+	free(*head);
+	head = NULL;
 }
