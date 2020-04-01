62b35b107e60023d9d05e4f2235177df73622f41
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-04-01 04:46:36 +0000

    fixing task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,26 +8,29 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
+	ssize_t let = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | OWRONLY, O6OO);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[len])
-		len++;
-
 	if (text_content == NULL)
 	{
-		return (-1);
+		text_content = "";
 	}
 	else
 	{
-		write(fd, text_content, len);
+		while (text_content[len])
+			len++;
+
+		let = write(fd, text_content, len);
 	}
+	if (let == -1)
+		return (-1);
 
 	close(fd);
 	return (1);
