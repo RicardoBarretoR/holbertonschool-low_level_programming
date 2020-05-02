e0e46d711e8d8fd498555bc5a1ae553c13b1c024
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-05-02 16:35:43 +0000

    adding conditional in case null to head

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,6 +10,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new;
 	dlistint_t *tmp;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
