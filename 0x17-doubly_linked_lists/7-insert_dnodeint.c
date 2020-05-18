d084d23083d0c2dba7ac9d22b62ff3ab958ffadd
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-05-18 03:56:58 +0000

    Added conditional head equal null and non-zero idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,39 +8,42 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *tmp1, *new;
-	unsigned int count;
+	dlistint_t *tmp, *tmp1 = NULL, *new;
+	unsigned int count = 0;
 
-	tmp1 = NULL;
-	count = 0;
-
-	if (*h == NULL && idx != 0)
-		return (NULL);
-
-	tmp = *h;
-	while (*h != NULL && count < idx)
-	{
-		if (count != 0)
-		{
-			if (tmp1 == NULL)
-			{
-				tmp1 = *h;
-			}
-			tmp1 = tmp1->next;
-		}
-		tmp = tmp->next;
-		count++;
-	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
-	new->n = n;
-	tmp1->next = new;
-	tmp->prev = new;
-
-	new->next = tmp;
-	new->prev = tmp1;
-
+	if (*h == NULL)
+	{
+		new->n = n;
+		new->next = NULL;
+		new->prev = NULL;
+	}
+	else
+	{
+		tmp = *h;
+		while (*h != NULL && count < idx)
+		{
+			if (count != 0)
+			{
+				if (tmp1 == NULL)
+					tmp1 = *h;
+				tmp1 = tmp1->next;
+			}
+			if (tmp->next != NULL)
+			{
+				tmp = tmp->next;
+				count++;
+			}
+			else
+				return (NULL);
+		}
+		new->n = n;
+		tmp1->next = new;
+		tmp->prev = new;
+		new->next = tmp;
+		new->prev = tmp1;
+	}
 	return (new);
 }
