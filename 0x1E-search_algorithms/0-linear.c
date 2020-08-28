58c5cc8f616c14f7b552685411bd0ab33d556e06
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-08-27 20:04:23 -0500

    fixing task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,17 +10,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t index;
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
 
-	for (index = 0; index < size; index++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-		if (array[index] == value)
-			return (index);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
