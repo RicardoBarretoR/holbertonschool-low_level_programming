306d6568da3d00e08527f2e693d1f493e96464e0
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-08-27 19:23:35 -0500

    fixing task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (index = 0; index != size; index++)
+	for (index = 0; index < size; index++)
 	{
 		if (array[index] == value)
 		{
